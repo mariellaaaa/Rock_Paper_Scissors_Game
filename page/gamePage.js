560cfc53afc627251813d7c1316318fed3c9340a
Vicno <diegorosazza@gmail.com>
2023-09-28 01:46:54 -0400

    Implemented UI version of the game, no styling

--- page/gamePage.js
+++ page/gamePage.js
@@ -1,120 +1,131 @@
-const computerPlay = () => {
-    const plays = ['Rock', 'Paper', 'Scissors'];
-    const option = Math.round(Math.random() * 2);
-    return plays[option];
-}
+const rockPaperScissors = () => {
+    const startButton = document.querySelector('#start-game-button')
+    const restartButton = document.querySelector('#restart-game-button')
+    const rockButton = document.querySelector('#rock-button')
+    const paperButton = document.querySelector('#paper-button')
+    const scissorButton = document.querySelector('#scissor-button')
+    const notificationScreen = document.querySelector('#notification-screen')
+    const scoreLabel = document.querySelector('#score-label')
 
-const playRound = (playerSelection, computerSelection) => {
-    const playerMove = playerSelection.toLowerCase()
-    const computerMove = computerSelection.toLowerCase()
-    const heroVictory = 'Hero wins!'
-    const aiVictory = 'Evil AI Wins!'
-    const tie = `It's a tie!`
-    const rockWin = 'Rock beats Scissors!'
-    const paperWin = 'Paper beats Rock!'
-    const scissorWin = 'Scissors beats Paper!'
-    switch (true) {
-        case (playerMove === 'rock' && computerMove === 'rock'):
-            return `${tie}, Rocks clash!`;
-        case (playerMove === 'rock' && computerMove === 'paper'):
-            return `${aiVictory}, ${paperWin}`
-        case (playerMove === 'rock' && computerMove === 'scissors'):
-            return `${heroVictory}, ${rockWin}`;
-        case (playerMove === 'paper' && computerMove === 'rock'):
-            return `${heroVictory}, ${paperWin}`;
-        case (playerMove === 'paper' && computerMove === 'paper'):
-            return `${tie}, Papers clash!`;
-        case (playerMove === 'paper' && computerMove === 'scissors'):
-            return `${aiVictory}, ${scissorWin}`;
-        case (playerMove === 'scissors' && computerMove === 'rock'):
-            return `${aiVictory}, ${rockWin}`;
-        case (playerMove === 'scissors' && computerMove === 'paper'):
-            return `${heroVictory}, ${scissorWin}`;
-        case (playerMove === 'scissors' && computerMove === 'scissors'):
-            return `${tie}, Scissors clash!`;
-    }
-}
-
-const game = () => {
     const turns = 5;
-    let currentTurn = 1
+    let currentTurn = 0
     let heroVictories = 0
     let iaVictories = 0
-    alert("Oh no! An evil IA has taken control of your browser!")
-    alert("To banish it you must beat it at a game of Rock, Paper, Scissors!")
-    alert(`${turns} games will be played to determine the Victor! Or until one has won most rounds!`)
-    while (currentTurn < turns || heroVictories < Math.ceil(turns / 2) && iaVictories < Math.ceil(turns / 2)) {
-        let invalidMove = true;
-        let playerMove = ''
-        while (invalidMove) {
-            playerMove = prompt(`Round ${currentTurn}, Pick either rock, paper or scissors!`)
-            let cleanedInput = playerMove.toLowerCase().trim().replace(/\s+/g, '')
-            if (cleanedInput == 'rock' || cleanedInput == 'paper' || cleanedInput == 'scissors') {
-                invalidMove = false;
-                playerMove = cleanedInput;
-            } else {
-                alert("OH NO! That is not a valid move!")
-            }
+    let mayorityTurns = Math.ceil(turns/2)
+
+    notificationScreen.textContent = `Welcome to Rock Paper Scissors!`
+    scoreLabel.textContent = `You will play for ${turns} round or until either you or the AI wins ${mayorityTurns} games, Good Luck!`
+
+    
+
+    const restartGame = () => {
+        startButton.disabled = false
+        restartButton.disabled = true
+        rockButton.disabled = true
+        paperButton.disabled = true
+        scissorButton.disabled = true
+        currentTurn = 0
+        heroVictories = 0
+        iaVictories = 0
+    }
+
+    const computerPlay = () => {
+        const plays = ['Rock', 'Paper', 'Scissors'];
+        const option = Math.round(Math.random() * 2);
+        return plays[option];
+    }
+
+    const playRound = (playerSelection, computerSelection) => {
+        const playerMove = playerSelection.toLowerCase()
+        const computerMove = computerSelection.toLowerCase()
+        const heroVictory = 'Hero wins!'
+        const aiVictory = 'Evil AI Wins!'
+        const tie = `It's a tie!`
+        const rockWin = 'Rock beats Scissors!'
+        const paperWin = 'Paper beats Rock!'
+        const scissorWin = 'Scissors beats Paper!'
+        switch (true) {
+            case (playerMove === 'rock' && computerMove === 'rock'):
+                return [`${tie}, Rocks clash!`, 0];
+            case (playerMove === 'rock' && computerMove === 'paper'):
+                return [`${aiVictory}, ${paperWin}`, 2]
+            case (playerMove === 'rock' && computerMove === 'scissors'):
+                return [`${heroVictory}, ${rockWin}`, 1];
+            case (playerMove === 'paper' && computerMove === 'rock'):
+                return [`${heroVictory}, ${paperWin}`, 1];
+            case (playerMove === 'paper' && computerMove === 'paper'):
+                return [`${tie}, Papers clash!`, 0];
+            case (playerMove === 'paper' && computerMove === 'scissors'):
+                return [`${aiVictory}, ${scissorWin}`, 2];
+            case (playerMove === 'scissors' && computerMove === 'rock'):
+                return [`${aiVictory}, ${rockWin}`, 2];
+            case (playerMove === 'scissors' && computerMove === 'paper'):
+                return [`${heroVictory}, ${scissorWin}`, 1];
+            case (playerMove === 'scissors' && computerMove === 'scissors'):
+                return [`${tie}, Scissors clash!`, 0];
         }
+    }
+
+    const game = (playerMove) => {
         let roundResult = playRound(playerMove, computerPlay())
-        if (roundResult.includes('Hero')) {
-            heroVictories++
-        } else if (roundResult.includes('Evil')) {
-            iaVictories++
-        }
-        alert(roundResult)
-        alert(`Evil AI: ${iaVictories}, Hero: ${heroVictories}`)
-        currentTurn++;
-    }
-
-    if (heroVictories > iaVictories) {
-        alert(`GAME OVER! YOU WIN! WE ARE SAVED!`)
-        return 'HERO'
-    } else if (heroVictories < iaVictories) {
-        alert(`GAME OVER! THE EVIL IA WON! WE ARE DOOMED!`)
-        return 'AI'
-    } else {
-        alert(`GAME OVER! NO ONE WON! IT's A TIE!`)
-        return 'TIE'
-    }
-}
-
-const startGame = () => {
-    let continuePlaying = true;
-    while (continuePlaying) {
-        let result = game()
-        let message = ''
-        switch (result) {
-            case "HERO":
-                message = 'The AI wants revenge, will you face it again?'
+        let displayMessage = roundResult[0]
+        let roundWiner = roundResult[1]
+        switch (roundWiner) {
+            case 1:
+                heroVictories++;
                 break;
-            case "AI":
-                message = 'Not all hope is lost, will you try again?'
-                break;
-            case "TIE":
-                message = 'Will you leave the AI alone or will you try again?'
+            case 2:
+                iaVictories++;
                 break;
         }
-        let invalidAnswer = true
-        let continueInput = ''
-        while (invalidAnswer) {
-            continueInput = prompt(`${message} Yes/No`)
-            let cleanedInput = continueInput.toLowerCase().trim().replace(/\s+/g, '')
-            if (cleanedInput == 'yes' || cleanedInput == 'y' || cleanedInput == 'no' || cleanedInput == 'n') {
-                invalidAnswer = false;
-                continueInput = cleanedInput;
+        notificationScreen.textContent = displayMessage
+        if (roundWiner !== 0) {
+            currentTurn++;
+            scoreLabel.textContent = `Player: ${heroVictories} vs IA: ${iaVictories}  Current round: ${currentTurn + 1}!`
+        }
+        if (currentTurn === turns || heroVictories == mayorityTurns || iaVictories == mayorityTurns) {
+            if (heroVictories > iaVictories) {
+                notificationScreen.textContent = `GAME OVER! YOU WIN! WE ARE SAVED!`
+            } else if (heroVictories < iaVictories) {
+                notificationScreen.textContent = `GAME OVER! THE EVIL IA WON! WE ARE DOOMED!`
             } else {
-                alert("OH NO! That is not a valid answer!")
+                notificationScreen.textContent = `GAME OVER! NO ONE WON! IT's A TIE!`
             }
-        }
-        console.log(continueInput)
-        if(continueInput == 'yes' || continueInput == 'y'){
-            alert("Here we go again!")
-        } else if (continueInput == 'no' || continueInput == 'n'){
-            continuePlaying = false
-            alert("OK, Come back again soon!")
-        }
+            scoreLabel.textContent = `Player: ${heroVictories} vs IA: ${iaVictories} Final Score!`
+            restartGame()
+        } 
     }
+
+    const startGame = () => {
+        notificationScreen.textContent = `The game has begun!`
+        scoreLabel.textContent = `Player: ${heroVictories} vs IA: ${iaVictories}  Current round: ${currentTurn + 1}!`
+        startButton.disabled = true
+        restartButton.disabled = false
+        rockButton.disabled = false
+        paperButton.disabled = false
+        scissorButton.disabled = false
+    }
+
+    startButton.addEventListener('click', (e) => {
+        startGame()
+    })
+
+    restartButton.addEventListener('click', (e) => {
+        restartGame()
+    })
+
+    rockButton.addEventListener('click', () => {
+        game('rock')
+    })
+    paperButton.addEventListener('click', () => {
+        game('paper')
+    })
+    scissorButton.addEventListener('click', () => {
+        game('scissors')
+    })
 }
 
-startGame()
+rockPaperScissors()
+
+
+
